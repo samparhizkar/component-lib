dfeb9caa1d17aad3bc967148b7c29d8b1306d43f
Sam Parhizkar <sparhizkar@nuvalence.io>
2020-10-16 11:14:16 -0400

    Fix enzyme type issues

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,17 @@ module.exports = {
   roots: ['src'],
   clearMocks: true,
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  testPathIgnorePatterns: ['dist/', 'lib/', 'example/', 'node_modules/'],
+  // testPathIgnorePatterns: ['dist/', 'lib/', 'example/', 'node_modules/'],
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es/.*)'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
   moduleNameMapper: {
     // Mocks out all these file formats when tests are run.
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
